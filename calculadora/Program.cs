271ea3964891a051c2f01300f790108ef4546271
Rubens Santo <r4454406@gmail.com>
2023-06-22 11:27:22 -0300

    Ajuste no codigo

--- calculadora/Program.cs
+++ calculadora/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            char operacao;
+            Console.Write("insira um número: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("insira outro número: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Insira a oprecao que deseja realizar(*,+,-,/):");
+            operacao = Convert.ToChar(Console.ReadLine());
+            switch (operacao)
+            {
+                case '*':
+                    Console.WriteLine("O resultado e: " + num1 * num2);
+                    break;
+                case '+':
+                    Console.WriteLine("O resultado e: " + (num1 + num2));
+                    break;
+                case '-':
+                    Console.WriteLine("O resultado e: " + (num1 - num2));
+                    break;
+                case '/':
+                    if (num2 != 0)
+                        Console.WriteLine("O resultado e: " + num1 / num2);
+                    else
+                        Console.WriteLine("Nao e possivel dividir por 0. ");
+                    break;
+                default:
+                    Console.WriteLine("Operacao invalida");
+                    break;
+
+            }
+
+        }
+    }
+}
+
